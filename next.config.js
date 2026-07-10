73ac9dca2d7f161ca200e58fa4c9f7fcd53e19de
pediaboy <thoriqpedia@gmail.com>
2026-07-11 03:47:48 +0700

    feat: add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'avatars.githubusercontent.com'],
+    domains: ["images.unsplash.com"],
   },
 }
-
 module.exports = nextConfig
